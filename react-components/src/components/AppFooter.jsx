5a8789d8b9312a97d6bb91157502f07b52502275
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-11-12 15:20:43 +0100

    ex-complete

--- react-components/src/components/AppFooter.jsx
+++ react-components/src/components/AppFooter.jsx
@@ -2,7 +2,10 @@ export default function AppFooter() {
     //markup
     return (
         <footer>
-            footer
+            <div className="text-footer">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illum aut soluta officia accusantium dolor excepturi blanditiis maiores optio repudiandae assumenda eligendi error facilis veritatis, consequuntur, unde aspernatur qui? Quos rerum blanditiis deserunt vel laboriosam natus officia tenetur ipsa ullam.
+            </div>
+
         </footer>
     )
 }
